9e6ba246cf26dd9c3c203164e107c62e21ce0f1e
Filipi Firmino <contatoic3web@gmail.com>
2021-03-26 07:36:56 -0300

    Setting context and adding css

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import storeConfig from './Store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter history={ createBrowserHistory() }>
+    <Provider store={ storeConfig() }>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
